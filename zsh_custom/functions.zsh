ae839b19cdc4eacbd8948435453ae1e2e0a1fbfc
Ben Burke <benjamindburke@gmail.com>
2024-08-15 12:41:01 -0400

    scripts that operate on all repos no longer change working dir

--- zsh_custom/functions.zsh
+++ zsh_custom/functions.zsh
@@ -1,20 +1,5 @@
 #!/usr/bin/env bash
 
-function recap_all() {
-    curdir=`pwd`
-    for dir in $( all_git_repos ); do
-        cd $dir
-
-        updates="$( recap_by_dates "$@" )"
-        if [[ ! -z "$updates" ]]; then
-            echo "[INFO] Repository: $dir"
-            echo "$updates"
-            echo
-        fi
-    done
-    cd $curdir
-}
-
 function recap()           { recap_by_range             "$@"; }
 function recap_day()       { recap_by_dates  --days=1   "$@"; }
 function recap_day_all()   { recap_all       --days=1   "$@"; }
